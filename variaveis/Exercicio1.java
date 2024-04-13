5229ca3bcd51a420757015228895a1664abaf696
jsmaryy <166922037+jsmaryy@users.noreply.github.com>
2024-04-13 14:12:49 +0000

    Aula 0

--- variaveis/Exercicio1.java
+++ variaveis/Exercicio1.java
@@ -1,5 +1,4 @@
 /*
-1. Crie uma variável que informa a primeira letra do seu nome;
 2. Crie uma variável que informa a sua idade;
 3. Crie uma variável informando se você tomou café da manhã;
 */
@@ -7,6 +6,15 @@
 public class Exercicio1 {
     public static void main(String[] args) {
         // Escreva seu código aqui ⬇️
+        char letraNome = 'M';
+        int idade = 16;
+        double alturaEmMetros = 1.57;
+        boolean tomouCafe = true;
+
+        idae += 10; -> idade = idade + 10;
+        idade ++;
+        idade + idade + 1;
+
 
 
     }
--- variaveis/Exercicio2.java
+++ variaveis/Exercicio2.java
@@ -4,9 +4,35 @@
 3. Some o valor da laranja com a uva;
 */
 
+import java.util.Scanner;
+
 public class Exercicio2 {
     public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
+        Scanner sc = new Scanner(System.in);
+
+        double precoLaranja = sc.nextDouble();
+        double precoUva = sc.nextDouble();
+
+        double soma = precoLaranja + precoUva;
+
+        System.out.println("preço Uva: "+precoUva);
+        System.out.println("preço Laranja: "+precoLaranja);
+        System.out.println(soma);
+        
+        // Recebam duas idades, faça a soma dela e imprima no terminal
+        int idade1 = sc.nextInt();
+        int idade2 = sc.nextInt();
+        int idade3 = 0;
+        idade3 = idade2 + idade1;
+
+        System.out.println("A soma entre as idades" +idade3); 
+        
+
+
+    
+
+
+
 
         
     }
